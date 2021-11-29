03d1ceec87ac2251619c422a168e28ec333105a0
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2021-11-29 21:19:49 +0530

    Update YTVideo.ts

--- src/commands/Media/YTVideo.ts
+++ src/commands/Media/YTVideo.ts
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         const video = new YT(M.urls[0], 'video')
         if (!video.validateURL()) return void M.reply(`Provide a Valid YT URL`)
         const { videoDetails } = await video.getInfo()
-        M.reply('「 Wait...」')
+        M.reply('「𝓛𝓸𝓪𝓭𝓲𝓷𝓰...」')
         if (Number(videoDetails.lengthSeconds) > 1800)
             return void M.reply('⚓ Cannot download videos longer than 30 minutes')
         M.reply(await video.getBuffer(), MessageType.video).catch((reason: Error) =>
