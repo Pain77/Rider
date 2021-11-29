8dc3a9f710f60fca6850d3a5763913726c04734f
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2021-11-29 21:47:39 +0530

    Update CallHandler.ts

--- src/Handlers/CallHandler.ts
+++ src/Handlers/CallHandler.ts
@@ -31,6 +31,6 @@ export default class CallHandler {
         ]
 
         await this.client.sendWA(`${tag},${JSON.stringify(json)}`)
-        await this.client.sendMessage(caller, `ᴄᴀʟʟ ᴀɢᴀɪɴ ꜰᴏʀ ᴛᴏ ɢᴇᴛ ʙᴀɴ🍁 .`, MessageType.text)
+        await this.client.sendMessage(caller, `ᴄᴀʟʟ ᴀɢᴀɪɴ ꜰᴏʀ ᴛᴏ ɢᴇᴛ ʙᴀɴ ♨️ .`, MessageType.text)
     }
 }
