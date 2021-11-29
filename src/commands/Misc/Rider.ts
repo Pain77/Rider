74fec235d173ed4f0a0b3904f60bcbd27489f18a
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2021-11-29 21:25:59 +0530

    Update Rider.ts

--- src/commands/Misc/Rider.ts
+++ src/commands/Misc/Rider.ts
@@ -8,7 +8,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'yatori',
+            command: 'Rider',
             description: 'Displays the info.',
             category: 'misc',
             usage: `${client.config.prefix}yatori`
@@ -17,12 +17,12 @@ export default class Command extends BaseCommand {
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
         const n = [
-            './assets/videos/well/well-git.mp4'
+            './assets/videos/pain/pain-git.mp4'
         ]
         let well = n[Math.floor(Math.random() * n.length)]
-        return void this.client.sendMessage(M.from, { url: well }, MessageType.video, {quoted:M.WAMessage,
+        return void this.client.sendMessage(M.from, { url: pain }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-           caption: `ʏᴀᴛᴏʀɪ 🍁!\n⚜ *Description: well...*\n` }
+           caption: `ʀɪᴅᴇʀ 🍁!\n⚜ *Description: Pain...*\n` }
         )
     }
 }
