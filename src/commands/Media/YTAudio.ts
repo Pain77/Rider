5142de87a844d8507c00e3b52fb0086583beca71
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2021-11-29 21:16:39 +0530

    Update YTAudio.ts

--- src/commands/Media/YTAudio.ts
+++ src/commands/Media/YTAudio.ts
@@ -21,7 +21,7 @@ export default class Command extends BaseCommand {
         if (!M.urls.length) return void M.reply('🔎 Provide the URL of the YT video you want to download')
         const audio = new YT(M.urls[0], 'audio')
         if (!audio.validateURL()) return void M.reply(`⚓ Provide a Valid YT URL`)
-        M.reply('〽「𝘓𝘰𝘥𝘪𝘯𝘨...」')
+        M.reply('♨️ 「𝓛𝓸𝓪𝓭𝓲𝓷𝓰...」')
         M.reply(await audio.getBuffer(), MessageType.audio).catch((reason: Error) =>
             M.reply(`❌ an error occurred, Reason: ${reason}`)
         )
