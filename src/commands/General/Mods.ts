6e1cc501cc9992b1785345b00ceb4ae2166cdf46
00300 <90952986+well300@users.noreply.github.com>
2021-12-02 12:11:24 +0530

    〽

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -20,9 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const kacchan =
-			"https://c.tenor.com/iv8N2t7pEXAAAAPo/bakugou-katsuki-anime.mp4";
-			"https://c.tenor.com/93eUTFPXSyYAAAPo/bakugo-my-hero-academia.mp4";
-			"https://c.tenor.com/LhRhrogP7WgAAAPo/bakugou-katsuki-bakugou.mp4";
+			"https://c.tenor.com/GbKP-leU6WsAAAPo/fate-stay.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: kacchan },
@@ -30,7 +28,7 @@ export default class Command extends BaseCommand {
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `🎗 ᴍᴏᴅᴇʀᴀᴛᴏʀꜱ \n╰• ᴄᴏɴᴛᴀᴄᴛ💣: wa.me/917842346461?text=𝙝𝙚𝙡𝙡𝙤`,
+				caption: `🎗 ᴍᴏᴅᴇʀᴀᴛᴏʀꜱ \n╰• ᴄᴏɴᴛᴀᴄᴛ💣: wa.me/919662713165?text=well`,
 			}
 		);
 	};
