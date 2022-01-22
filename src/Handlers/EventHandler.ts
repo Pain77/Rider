5f46e2e6f930e0531652a8f9365c27b4ef8426ed
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-22 13:17:26 +0530

    Update EventHandler.ts

--- src/Handlers/EventHandler.ts
+++ src/Handlers/EventHandler.ts
@@ -16,15 +16,16 @@ export default class EventHandler {
         const data = await this.client.getGroupData(event.jid)
         if (!data.events) return void null
         const add = event.action === 'add'
+        const remove = event.action === 'remove'
         const text = add
-            ? `⁍ 𝙒𝙚𝙡𝙘𝙤𝙢𝙚 𝙩𝙤 - ${group.subject || '___'} -\n\n🍁ɢʀᴏᴜᴘ ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ:\n${
+            ? `⪢ 𝙒𝙚𝙡𝙘𝙤𝙢𝙚 𝙩𝙤 - ${group.subject || '___'} -\n\n🍁ɢʀᴏᴜᴘ ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ:\n${
                group.desc
              }\n\n🎗ʜᴏᴘᴇ ʏᴏᴜ ꜰᴏʟʟᴏᴡ ᴛʜᴇ ʀᴜʟᴇꜱ ᴀɴᴅ ʜᴀᴠᴇ ꜰᴜɴ!\n\n*‣ ${event.participants
               .map((jid) => `@${jid.split('@')[0]}`)
               .join(', ')}*`
               
             : event.action === 'remove'
-            ? `*@${event.participants[0].split('@')[0]}* ʜᴀꜱ ʟᴇꜰᴛ ᴛʜᴇ ᴄʜᴀᴛ ᴀɴᴅ ɪɢ ᴡᴇ ᴅᴏɴ'ᴛ ɴᴇᴇᴅ ʜᴇʀ/ʜɪᴍ ʙᴀᴄᴋ ᴀɢᴀɪɴ 🌝`
+            ? `*@${event.participants[0].split('@')[0]}* ʜᴀꜱ ʟᴇꜰᴛ ᴛʜᴇ ᴄʜᴀᴛ ᴀɴᴅ ᴡᴇ'ʟʟ ᴍɪꜱꜱ ʏᴏᴜ!`
             : `*@${event.participants[0].split('@')[0]}* got ${this.client.util.capitalize(event.action)}d${
                   event.actor ? ` by *@${event.actor.split('@')[0]}*` : ''
               }`
@@ -33,6 +34,16 @@ export default class EventHandler {
         }
         if (add) {
             let image = await this.client.assets.get('welcome1')
+            
+            if (image)
+                return void (await this.client.sendMessage(event.jid, image, MessageType.video, {
+                    caption: text,
+                    mimetype: Mimetype.gif,
+                    contextInfo
+                }))
+        }
+        if (remove) {
+            let image = await this.client.assets.get('goodbye1')
             
             if (image)
                 return void (await this.client.sendMessage(event.jid, image, MessageType.video, {
