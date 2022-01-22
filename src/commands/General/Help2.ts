a72fa55c4c153a421c4f32aa4e4b22077e3df72d
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-22 13:27:55 +0530

    Update Help2.ts

--- src/commands/General/Help2.ts
+++ src/commands/General/Help2.ts
@@ -12,18 +12,18 @@ export default class Command extends BaseCommand {
 			command: "help2",
 			description: "Displays the info",
 			category: "general",
-			usage: `${client.config.prefix}kacchan`,
+			usage: `${client.config.prefix}rider`,
 		        dm: true,
-                        aliases: ['h2','?2','well2','menu2']
+                        aliases: ['h2','?2','menu2']
 		});
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const kacchan = 
+		const rider = 
 			"https://c.tenor.com/iM-nb2_7qcIAAAPo/rider-medusa.mp4";
 		return void this.client.sendMessage(
 			M.from,
-			{ url: kacchan },
+			{ url: rider },
 			MessageType.video,
 			{
 				quoted: M.WAMessage,
